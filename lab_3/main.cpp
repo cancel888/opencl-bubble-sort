54c85ccc63f759b5ee302b3b3be8268357429c1a
cancel <sl888amg@gmail.com>
2015-04-25 13:16:09 +0300

    Initial Commit

---
+++ lab_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_3
+//
+//  Created by Nikita on 25.04.15.
+//  Copyright (c) 2015 Nikita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
